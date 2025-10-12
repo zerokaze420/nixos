927e730288aa6f3efb98b1392bf0a41dc5324fbb
ZeroKaze <erfu81856@openatom.club>
2025-10-12 19:40:36 +0800

    f

--- homeModules/desktop/kitty.nix
+++ homeModules/desktop/kitty.nix
@@ -2,14 +2,5 @@
 {
     programs.kitty = {
     enable = true;
-    fontSize = 14.0;
-    fonts = with pkgs; [
-        fira-code
-        fira-code-symbols
-        mplus-outline-fonts.githubRelease
-        dina-font
-        proggyfonts
-        font-awesome
-    ];
-    }
+    };
 }
