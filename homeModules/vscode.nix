38b23d07e65d6ddd8146d9c27fa42022dc90de2b
ZeroKaze <erfu81856@openatom.club>
2025-10-12 19:28:59 +0800

    f

--- homeModules/vscode.nix
+++ homeModules/vscode.nix
@@ -1,4 +1,13 @@
 {config, pkgs, ... }:
 {
- programs.vscode.enable = true;
+
+ programs.vscode = {
+  enable = true;
+  extensions = with pkgs.vscode-extensions; [
+    dracula-theme.theme-dracula
+    vscodevim.vim
+    yzhang.markdown-all-in-one
+  ];
+};
+
 }
