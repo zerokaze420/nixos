1626c160604aab729cf775a00dcce100a8320283
erfu81856 <erfu81856@openatom.club>
2025-11-10 16:20:16 +0800

    feat(helix): add Rust language configuration and enable helix program

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@
     ./homeModules/vscode.nix
     ./homeModules/shell/starship.nix
     ./homeModules/desktop/kitty.nix
+    ./homeModules/desktop/helix.nix
     inputs.zen-browser.homeModules.beta
   ];
 
@@ -48,46 +49,6 @@
   programs.dankMaterialShell.enable = true;
   programs.zen-browser.enable = true;
 
-  programs.helix = {
-    enable = true;
-    settings = {
-      theme = "autumn_night_transparent";
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
-      };
-    };
-
-    # 3. ADD RUST LANGUAGE CONFIGURATION HERE
-    languages.language = [
-      # Existing Nix config
-      {
-        name = "nix";
-        auto-format = true;
-        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-      }
-      # New Rust config
-      {
-        name = "rust";
-        auto-format = true;
-        language-servers = [ "rust-analyzer" ];
-      }
-    ];
-
-    # 4. Configure the language server (optional, but good practice)
-    languages.language-server.rust-analyzer.config = {
-      # This tells rust-analyzer to run clippy instead of cargo check on save
-      checkOnSave.command = "clippy";
-    };
-
-    themes = {
-      autumn_night_transparent = {
-        "inherits" = "autumn_night";
-        "ui.background" = { };
-      };
-    };
-  };
 
   home.stateVersion = "23.11";
 }
